7284fa43013bb2d733a8818ab994c09a491fb503
PoXuanla <wdftmmas234@gmail.com>
2022-04-10 19:58:45 +0800

    feat 調整server.js 命名 將server.js 改名為 index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const app = require('./app')
+const mongoose = require('mongoose')
+
+const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD)
+
+mongoose
+  .connect(DB, {
+    // useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+    // useUnifiedTopology: true,
+  })
+  .then((con) => {
+    console.log('DB connection successful!')
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+
+const PORT = 5000
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`)
+})
